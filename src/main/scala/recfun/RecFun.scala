bb8b9f414ee3b0d4bbdf0a3efe2b5a049476a8a1
Jonathan Gordon <jonathan.gordon@nielsen.com>
2020-02-13 16:29:02 -0800

    non recursive solution

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -14,7 +14,21 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    val my2dArray = Array.ofDim[Int](r + 1, r + 1)
+    for (row <- 0 to r) {
+      for (col <- 0 to row) {
+        if (col == row || col == 0) {
+          my2dArray(row)(col) = 1
+        }
+        else {
+          my2dArray(row)(col) = my2dArray(row - 1)(col - 1) + my2dArray(row - 1)(col)
+        }
+      }
+    }
+
+    my2dArray(r)(c)
+  }
 
   /**
    * Exercise 2
